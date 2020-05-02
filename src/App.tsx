63a82d3c709ddd16671b53cf57ea9046ed29db90
mizdra <pp.mizdra@gmail.com>
2020-05-02 13:48:10 +0900

    refactor: モジュールに切り出す

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useCallback, useState, useEffect, useRef } from 'react';
+import React, { useMemo, useCallback, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import { useCascadeTimer } from './hook/use-cascade-timer';
 import { TimerRemainDisplay } from './component/TimerRemainDisplay';
@@ -6,10 +6,7 @@ import { TimerController } from './component/TimerController';
 import { TimerTimeline } from './component/TimerTimeline';
 import { TimerConfigForm, TimerConfig } from './component/TimerConfigForm';
 import { useOffsetChangeShortcut } from './hook/use-offset-change-shortcut';
-import { CascadeTimerStatus } from './lib/timer/cascade-timer';
-import { formatDuration } from './lib/timer/duration';
-import tickTackAudioPath from './audio/ticktack.mp3';
-import endedAudioPath from './audio/ended.mp3';
+import { useSoundEffect } from './hook/use-sound-effect';
 
 const DEFAULT_LAP_CONFIGS: TimerConfig['laps'] = [
   { title: 'お湯が沸くまで', duration: 3 * 1000 },
@@ -18,33 +15,6 @@ const DEFAULT_LAP_CONFIGS: TimerConfig['laps'] = [
   { title: '定時まで', duration: 10 * 1000 },
   { title: '就寝まで', duration: 10000000 * 1000 },
 ];
-
-function usePrevious<T>(value: T) {
-  const ref = useRef<T>();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
-
-function useAlarm(status: CascadeTimerStatus, currentLapRemain: number, currentLapIndex: number) {
-  const tickTackAudio = useMemo(() => new Audio(tickTackAudioPath), []);
-  const endedAudio = useMemo(() => new Audio(endedAudioPath), []);
-  const { seconds } = formatDuration(currentLapRemain);
-  const prevStatus = usePrevious(status);
-  const prevCurrentLapIndex = usePrevious(currentLapIndex);
-  const prevSeconds = usePrevious(seconds);
-
-  useEffect(() => {
-    if (prevStatus === 'countdowning' && status === 'ended') {
-      endedAudio.play();
-    } else if (status === 'countdowning' && prevCurrentLapIndex !== currentLapIndex) {
-      endedAudio.play();
-    } else if (status === 'countdowning' && prevSeconds !== seconds && seconds < 10) {
-      tickTackAudio.play();
-    }
-  }, [currentLapIndex, endedAudio, prevCurrentLapIndex, prevSeconds, prevStatus, seconds, status, tickTackAudio]);
-}
 
 export type AppProps = {};
 
@@ -66,7 +36,7 @@ export function App(_props: AppProps) {
 
   useOffsetChangeShortcut(offset, setOffset);
 
-  useAlarm(status, currentLapRemain, currentLapIndex);
+  useSoundEffect(status, currentLapRemain, currentLapIndex);
 
   const handleConfigSave = useCallback((config: TimerConfig) => {
     setLapConfigs(config.laps);
