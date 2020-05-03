25ac20c806c97ee6659df60bd8d5418b8d4daeb5
mizdra <pp.mizdra@gmail.com>
2020-05-03 16:21:44 +0900

    refactor: hooksの返り値を分割代入せずにパフォーマンスに影響のない範囲で持ち回す

--- src/hook/use-sound-effect.ts
+++ src/hook/use-sound-effect.ts
@@ -1,12 +1,13 @@
 import { useMemo, useEffect } from 'react';
-import { CascadeTimerStatus } from '../lib/timer/cascade-timer';
 import { formatDuration } from '../lib/timer/duration';
 import tickTackAudioPath from '../audio/ticktack.mp3';
 import endedAudioPath from '../audio/ended.mp3';
 import { usePrevious } from './use-previous';
+import { UseCascadeTimerResult } from './use-cascade-timer';
 
 /** 秒の位が変わる時と残り時間が 0 になった時に音を鳴らす */
-export function useSoundEffect(status: CascadeTimerStatus, currentLapRemain: number, currentLapIndex: number) {
+export function useSoundEffect(timer: UseCascadeTimerResult) {
+  const { status, currentLapRemain, currentLapIndex } = timer;
   const tickTackAudio = useMemo(() => new Audio(tickTackAudioPath), []);
   const endedAudio = useMemo(() => new Audio(endedAudioPath), []);
   const { seconds } = formatDuration(currentLapRemain);
