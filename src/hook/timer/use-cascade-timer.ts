0e760fa620d77bfe202506be900dd7160f4bedb2
mizdra <pp.mizdra@gmail.com>
2020-07-22 01:12:07 +0900

    useCascadeTimerからrecoil stateを使う

--- src/hook/timer/use-cascade-timer.ts
+++ src/hook/timer/use-cascade-timer.ts
@@ -1,72 +1,41 @@
-import { useState, useCallback, useEffect, useMemo } from 'react';
-import { CascadeTimer, CascadeTimerStatus } from '../../lib/timer/cascade-timer';
+import { useCallback, useEffect, useMemo } from 'react';
+import { CascadeTimer } from '../../lib/timer/cascade-timer';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import {
+  statusState,
+  currentLapRemainState,
+  currentLapIndexState,
+  offsetState,
+  lapDurationsState,
+} from '../../recoil/cascade-timer';
 
-/**
- * タイマーの開始時刻を基準とした各ラップが終了するまでの所要時間を返す.
- * @example `lapDurations` が `[1, 2, 3]` の時, `[1, 3, 6]` を返す.
- * */
-function calcLapEndTimes(lapDurations: number[]): number[] {
-  const lapEndTimes = new Array<number>(lapDurations.length);
-  let acc = 0;
-  for (let i = 0; i < lapDurations.length; i++) {
-    acc += lapDurations[i];
-    lapEndTimes[i] = acc;
-  }
-  return lapEndTimes;
-}
-
-/** 総経過時間を算出する */
-function calcTotalElapsed(lapDurations: number[], currentLapRemain: number, currentLapIndex: number): number {
-  let elapsed = 0;
-  for (let i = 0; i < currentLapIndex; i++) {
-    elapsed += lapDurations[i];
-  }
-  elapsed += lapDurations[currentLapIndex] - currentLapRemain;
-  return elapsed;
-}
-
-export type UseCascadeTimerResult = CascadeTimerState & {
-  /**
-   * タイマーの開始時刻を基準とした各ラップが終了するまでの所要時間.
-   * @example `lapDurations` が `[1, 2, 3]` の時, `lapEndTimes` は `[1, 3, 6]` となる.
-   * */
-  lapEndTimes: number[];
+export type UseCascadeTimerResult = {
   start: () => void;
   reset: () => void;
   setOffset: (newOffset: number) => void;
 };
 
-type CascadeTimerState = {
-  status: CascadeTimerStatus;
-  currentLapRemain: number;
-  currentLapIndex: number;
-  /** タイマーを開始してから現在までの経過時間. 値は `tick` イベントに合わせて更新される. */
-  totalElapsed: number;
-  offset: number;
-};
+export function useCascadeTimer(): UseCascadeTimerResult {
+  const setStatus = useSetRecoilState(statusState);
+  const setCurrentLapRemain = useSetRecoilState(currentLapRemainState);
+  const setCurrentLapIndex = useSetRecoilState(currentLapIndexState);
+  const setOffset = useSetRecoilState(offsetState);
 
-export function useCascadeTimer(lapDurations: number[]): UseCascadeTimerResult {
-  const { timer, lapEndTimes } = useMemo(() => {
+  const lapDurations = useRecoilValue(lapDurationsState);
+
+  const timer = useMemo(() => {
     const timer = new CascadeTimer(lapDurations);
-    const lapEndTimes = calcLapEndTimes(lapDurations);
-    return { timer, lapEndTimes };
+    return timer;
   }, [lapDurations]);
 
-  // 状態と状態更新用の util 関数を定義
-  const [state, setState] = useState<CascadeTimerState>(() => {
-    const state = timer.getState();
-    return {
-      ...state,
-      totalElapsed: calcTotalElapsed(lapDurations, state.currentLapRemain, state.currentLapIndex),
-    };
-  });
+  // 状態更新用の util 関数を定義
   const syncStateWithTimer = useCallback(() => {
     const state = timer.getState();
-    setState({
-      ...state,
-      totalElapsed: calcTotalElapsed(lapDurations, state.currentLapRemain, state.currentLapIndex),
-    });
-  }, [lapDurations, timer]);
+    setStatus(state.status);
+    setCurrentLapRemain(state.currentLapRemain);
+    setCurrentLapIndex(state.currentLapIndex);
+    setOffset(state.offset);
+  }, [setCurrentLapIndex, setCurrentLapRemain, setOffset, setStatus, timer]);
 
   // タイマーを操作するAPIを定義
   const start = useCallback(() => {
@@ -77,7 +46,7 @@ export function useCascadeTimer(lapDurations: number[]): UseCascadeTimerResult {
     timer.reset();
     syncStateWithTimer();
   }, [syncStateWithTimer, timer]);
-  const setOffset = useCallback(
+  const setOffsetForOuter = useCallback(
     (newOffset: number) => {
       timer.setOffset(newOffset);
       syncStateWithTimer();
@@ -96,5 +65,5 @@ export function useCascadeTimer(lapDurations: number[]): UseCascadeTimerResult {
     return unsubscribe;
   }, [lapDurations, syncStateWithTimer, timer]);
 
-  return { ...state, lapEndTimes, start, reset, setOffset };
+  return { start, reset, setOffset: setOffsetForOuter };
 }
