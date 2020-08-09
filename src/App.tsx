4ee6c5aa829b06128e39dd40d40321d0d092b6a2
mizdra <pp.mizdra@gmail.com>
2020-08-09 19:31:42 +0900

    メモリ撹拌の調査のため、一時的にタイムラインを無効化

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Container from '@material-ui/core/Container';
 import { useCascadeTimer } from './hook/timer/use-cascade-timer';
 import { TimerRemainDisplay } from './component/molecules/timer/TimerRemainDisplay';
 import { TimerController } from './component/molecules/timer/TimerController';
-import { TimerTimeline } from './component/molecules/timer/TimerTimeline';
 import { TimerConfigForm, TimerConfig } from './component/molecules/timer/TimerConfigForm';
 import { useSetRecoilState } from 'recoil';
 import { lapConfigsState } from './recoil/cascade-timer';
@@ -24,7 +23,6 @@ export function App(_props: AppProps) {
   return (
     <Container maxWidth="lg" style={{ padding: 30 }}>
       <TimerConfigForm onSave={handleConfigSave} />
-      <TimerTimeline />
       <TimerRemainDisplay setOffset={timer.setOffset} />
       <TimerController onStart={timer.start} onStop={timer.reset} />
     </Container>
