e2acf98c7f5ef478f933ff69f2dc7e2823f85117
mizdra <pp.mizdra@gmail.com>
2020-09-27 14:35:47 +0900

    古いタイムラインコンポーネントを置き換える

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import 'vis-timeline/styles/vis-timeline-graph2d.css';
 import { RecoilRoot } from 'recoil';
 
 render(
