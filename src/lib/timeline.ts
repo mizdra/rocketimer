e55481be125066cc70f82b12693a34a8336422ee
mizdra <pp.mizdra@gmail.com>
2020-09-27 15:54:36 +0900

    リファクタリング

--- src/lib/timeline.ts
+++ src/lib/timeline.ts
@@ -24,29 +24,31 @@ export type LapEndLine = {
   x: Px;
 };
 
-export type GridConfig = {
-  unit: 'day' | 'hour' | 'minute' | 'second';
-  value: Ms;
+export type ScaleConfig = {
+  msByPx: number;
+  gridStepUnit: 'day' | 'hour' | 'minute' | 'second';
+  gridStepTime: Ms;
 };
 
-function calcGridConfig(zoom: Ms): GridConfig {
-  if (zoom >= DAY) return { unit: 'day', value: 1 * DAY };
-  if (zoom >= HOUR * 4) return { unit: 'hour', value: 4 * HOUR };
-  if (zoom >= HOUR) return { unit: 'hour', value: 1 * HOUR };
-  if (zoom >= MINUTE * 15) return { unit: 'minute', value: 15 * MINUTE };
-  if (zoom >= MINUTE * 10) return { unit: 'minute', value: 10 * MINUTE };
-  if (zoom >= MINUTE * 5) return { unit: 'minute', value: 5 * MINUTE };
-  if (zoom >= MINUTE) return { unit: 'minute', value: 1 * MINUTE };
-  if (zoom >= SECOND * 15) return { unit: 'second', value: 15 * SECOND };
-  if (zoom >= SECOND * 10) return { unit: 'second', value: 10 * SECOND };
-  if (zoom >= SECOND * 5) return { unit: 'second', value: 5 * SECOND }; // default (5000 <= zoom < 10000)
-  return { unit: 'second', value: 1 * SECOND };
+function calcScaleConfig(zoom: Ms): ScaleConfig {
+  const msByPx = zoom / 225;
+  if (zoom >= DAY) return { msByPx, gridStepUnit: 'day', gridStepTime: 1 * DAY };
+  if (zoom >= HOUR * 4) return { msByPx, gridStepUnit: 'hour', gridStepTime: 4 * HOUR };
+  if (zoom >= HOUR) return { msByPx, gridStepUnit: 'hour', gridStepTime: 1 * HOUR };
+  if (zoom >= MINUTE * 15) return { msByPx, gridStepUnit: 'minute', gridStepTime: 15 * MINUTE };
+  if (zoom >= MINUTE * 10) return { msByPx, gridStepUnit: 'minute', gridStepTime: 10 * MINUTE };
+  if (zoom >= MINUTE * 5) return { msByPx, gridStepUnit: 'minute', gridStepTime: 5 * MINUTE };
+  if (zoom >= MINUTE) return { msByPx, gridStepUnit: 'minute', gridStepTime: 1 * MINUTE };
+  if (zoom >= SECOND * 15) return { msByPx, gridStepUnit: 'second', gridStepTime: 15 * SECOND };
+  if (zoom >= SECOND * 10) return { msByPx, gridStepUnit: 'second', gridStepTime: 10 * SECOND };
+  if (zoom >= SECOND * 5) return { msByPx, gridStepUnit: 'second', gridStepTime: 5 * SECOND }; // default (5000 <= zoom < 10000)
+  return { msByPx, gridStepUnit: 'second', gridStepTime: 1 * SECOND };
 }
 
-function calcGridLabel(gridConfig: GridConfig, gridLineTime: Ms): string {
-  if (gridConfig.unit == 'day') return (Math.round(gridLineTime / DAY) % 100) + 'd';
-  if (gridConfig.unit == 'hour') return (Math.round(gridLineTime / HOUR) % 100) + 'h';
-  if (gridConfig.unit == 'minute') return (Math.round(gridLineTime / MINUTE) % 60) + 'm';
+function calcGridLabel(gridConfig: ScaleConfig, gridLineTime: Ms): string {
+  if (gridConfig.gridStepUnit == 'day') return (Math.round(gridLineTime / DAY) % 100) + 'd';
+  if (gridConfig.gridStepUnit == 'hour') return (Math.round(gridLineTime / HOUR) % 100) + 'h';
+  if (gridConfig.gridStepUnit == 'minute') return (Math.round(gridLineTime / MINUTE) % 60) + 'm';
   return (Math.round(gridLineTime / SECOND) % 60) + 's';
 }
 
@@ -57,21 +59,20 @@ function calcRangeConfig(msByPx: number, elapsed: Ms, stageWidth: Px): { minTime
 }
 
 export function calcFloatingObjects(stageWidth: Px, zoom: Ms, elapsed: Ms, lapEndTimes: number[]) {
-  const msByPx = zoom / 225;
-  const gridConfig = calcGridConfig(zoom); // step: 10s
-  const { minTime, maxTime } = calcRangeConfig(msByPx, elapsed, stageWidth); // 20s, 60s
-  const minGridLineTime = Math.floor(minTime / gridConfig.value) * gridConfig.value;
+  const scaleConfig = calcScaleConfig(zoom); // step: 10s
+  const { minTime, maxTime } = calcRangeConfig(scaleConfig.msByPx, elapsed, stageWidth); // 20s, 60s
+  const minGridLineTime = Math.floor(minTime / scaleConfig.gridStepTime) * scaleConfig.gridStepTime;
 
   function timeToX(time: Ms) {
-    return CURRENT_LINE_X + (time - elapsed) / msByPx;
+    return CURRENT_LINE_X + (time - elapsed) / scaleConfig.msByPx;
   }
 
   const grids: Grid[] = [];
-  for (let gridLineTime = minGridLineTime; gridLineTime <= maxTime; gridLineTime += gridConfig.value) {
+  for (let gridLineTime = minGridLineTime; gridLineTime <= maxTime; gridLineTime += scaleConfig.gridStepTime) {
     const lineX = timeToX(gridLineTime);
     grids.push({
       time: gridLineTime,
-      labelText: calcGridLabel(gridConfig, gridLineTime),
+      labelText: calcGridLabel(scaleConfig, gridLineTime),
       lineX: lineX,
       labelX: lineX + 5,
     });
