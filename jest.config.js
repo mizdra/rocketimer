677a6b0c00a835e33a85d0a2883cbd9b3a71b175
mizdra <pp.mizdra@gmail.com>
2020-09-28 00:09:00 +0900

    mp3 の import で jest が落ちる問題を修正

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
   collectCoverageFrom: ['src/lib/**/*.ts'],
+  moduleNameMapper: {
+    '\\.mp3$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
