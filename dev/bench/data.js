1e318a7bd2e6871626cebe06075eaf5e86c8ffe7
github-action-benchmark <github@users.noreply.github.com>
2020-10-01 14:27:57 +0000

    add Benchmark (benchmarkjs) benchmark result for 236b1f5bdbd4e80cb5b5ed7f5ed365709a621a80

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1601562477711,
+  "repoUrl": "https://github.com/RNGeek/rocketimer",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "pp.mizdra@gmail.com",
+            "name": "mizdra",
+            "username": "mizdra"
+          },
+          "committer": {
+            "email": "pp.mizdra@gmail.com",
+            "name": "mizdra",
+            "username": "mizdra"
+          },
+          "distinct": true,
+          "id": "236b1f5bdbd4e80cb5b5ed7f5ed365709a621a80",
+          "message": "test",
+          "timestamp": "2020-10-01T23:26:41+09:00",
+          "tree_id": "9906508074c131f36ed7a5608d19693feccb58c8",
+          "url": "https://github.com/RNGeek/rocketimer/commit/236b1f5bdbd4e80cb5b5ed7f5ed365709a621a80"
+        },
+        "date": 1601562477324,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1192699,
+            "range": "±1.36%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 10066,
+            "range": "±1.36%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
