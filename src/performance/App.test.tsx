19e6fac589763f675b9dc9a8691a79f379c10b43
mizdra <pp.mizdra@gmail.com>
2020-10-02 01:31:24 +0900

    誤差範囲を表示する

--- src/performance/App.test.tsx
+++ src/performance/App.test.tsx
@@ -7,8 +7,55 @@ import { RecoilRoot } from 'recoil';
 import { TestableTimerController } from '../lib/timer/timer-controller';
 import { promises as fs } from 'fs';
 
-function average(nums: number[]): number {
-  return nums.reduce((a, b) => a + b, 0) / nums.length;
+// !!! The following code is copied from benchmark.js !!!
+// this code is lisenced by LICENSE.BENCHMARKJS
+const tTable = {
+  '1': 12.706,
+  '2': 4.303,
+  '3': 3.182,
+  '4': 2.776,
+  '5': 2.571,
+  '6': 2.447,
+  '7': 2.365,
+  '8': 2.306,
+  '9': 2.262,
+  '10': 2.228,
+  '11': 2.201,
+  '12': 2.179,
+  '13': 2.16,
+  '14': 2.145,
+  '15': 2.131,
+  '16': 2.12,
+  '17': 2.11,
+  '18': 2.101,
+  '19': 2.093,
+  '20': 2.086,
+  '21': 2.08,
+  '22': 2.074,
+  '23': 2.069,
+  '24': 2.064,
+  '25': 2.06,
+  '26': 2.056,
+  '27': 2.052,
+  '28': 2.048,
+  '29': 2.045,
+  '30': 2.042,
+  infinity: 1.96,
+};
+
+function getStatistics(samples: number[]) {
+  const size = samples.length;
+  // 更新時間の平均
+  const mean = samples.reduce((a, b) => a + b, 0) / size;
+  const variance = samples.reduce((sum, x) => sum + (x - mean) ** 2, 0) / (size - 1);
+  const sd = Math.sqrt(variance);
+  const sem = sd / Math.sqrt(size);
+  const df = size - 1;
+  const critical = tTable[((Math.round(df) || 1) as unknown) as keyof typeof tTable] || tTable.infinity;
+  const moe = sem * critical;
+  // t分布に基づく誤差範囲
+  const rme = (moe / mean) * 100 || 0;
+  return { mean, rme };
 }
 
 // 測定の際に何回コンポーネントを更新するか
@@ -83,15 +130,19 @@ test('タイマーが 60 fps で描画されることをテストする', async 
   const updatesForTimerRemainDisplay = renderTime.current.TimerRemainDisplay.updates.slice(
     -UPDATE_COUNT_FOR_MEASUREMENT,
   );
+  const statForTimerTimeline = getStatistics(updatesForTimerTimeline);
+  const statForTimerRemainDisplay = getStatistics(updatesForTimerRemainDisplay);
 
   // github-action-benchmark 向けに結果を書き出す
   await fs.writeFile(
     'output.txt',
     `
-TimerTimeline x ${average(updatesForTimerTimeline)} ms/render ±0.00% (${UPDATE_COUNT_FOR_MEASUREMENT} runs sampled)
-TimerRemainDisplay x ${average(
-      updatesForTimerRemainDisplay,
-    )} ms/render ±0.00% (${UPDATE_COUNT_FOR_MEASUREMENT} runs sampled)
+TimerTimeline x ${statForTimerTimeline.mean} ms/render ±${statForTimerTimeline.rme.toFixed(
+      2,
+    )}% (${UPDATE_COUNT_FOR_MEASUREMENT} runs sampled)
+TimerRemainDisplay x ${statForTimerRemainDisplay.mean} ms/render ±${statForTimerRemainDisplay.rme.toFixed(
+      2,
+    )}% (${UPDATE_COUNT_FOR_MEASUREMENT} runs sampled)
   `.trim(),
   );
 
