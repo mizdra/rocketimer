8fa784f0812ce97e46bd19e79e8e1573b9390d2b
mizdra <pp.mizdra@gmail.com>
2020-10-03 16:02:37 +0900

    perfect draw を無効化

--- src/component/molecules/timer/TimerTimeline.tsx
+++ src/component/molecules/timer/TimerTimeline.tsx
@@ -112,6 +112,7 @@ function useKonvaCanvas(ref: React.RefObject<HTMLDivElement>) {
           node.transformsEnabled('position');
           node.hitStrokeWidth(0);
           node.shadowForStrokeEnabled(false);
+          node.perfectDrawEnabled(false);
         }
       });
     });
