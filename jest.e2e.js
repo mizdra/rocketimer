e329dd8ce397c31a9fa41d501fd380bf620a796f
mizdra <pp.mizdra@gmail.com>
2020-10-12 01:36:28 +0900

    生で playwright 叩けば良いので jest-playwright-preset は使わない

--- jest.e2e.js
+++ jest.e2e.js
@@ -3,6 +3,5 @@
 /** @type import('@jest/types').Config.InitialOptions */
 module.exports = {
   displayName: 'e2e',
-  preset: 'jest-playwright-preset',
   testMatch: ['<rootDir>/e2e/**/*.test.ts?(x)'],
 };
