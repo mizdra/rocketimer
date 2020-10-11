7722a99e8c4a2f7de8e2f5440fd832e531360654
mizdra <pp.mizdra@gmail.com>
2020-10-11 20:33:27 +0900

    ベンチマークが動かせるよう npm-script を整理

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,25 @@
-module.exports = {
-  testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
-  collectCoverageFrom: ['src/lib/**/*.ts'],
+// @ts-check
+
+/** @type import('@jest/types').Config.InitialOptions */
+const SHARED_CONFIG = {
   moduleNameMapper: {
     '\\.mp3$': '<rootDir>/__mocks__/fileMock.js',
   },
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx,js,jsx}', '!**/*.d.ts'],
+};
+
+/** @type import('@jest/types').Config.InitialOptions */
+module.exports = {
+  projects: [
+    {
+      ...SHARED_CONFIG,
+      displayName: 'test',
+      testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
+      collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx,js,jsx}', '!**/*.d.ts'],
+    },
+    {
+      ...SHARED_CONFIG,
+      displayName: 'benchmark',
+      testMatch: ['<rootDir>/benchmark/render-time.tsx'],
+    },
+  ],
 };
