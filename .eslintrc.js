cd049131c9223945dc2bc805bff7d5a0f8e5df06
mizdra <pp.mizdra@gmail.com>
2020-10-12 00:00:50 +0900

    eslint-config-mizdra を使う

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
   root: true,
-  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ['@mizdra/mizdra', '@mizdra/mizdra/+typescript', '@mizdra/mizdra/+react', '@mizdra/mizdra/+prettier'],
   parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
     project: ['./tsconfig.frontend.json', './tsconfig.benchmark.json'],
   },
   env: {
@@ -15,86 +10,7 @@ module.exports = {
     node: true, // for jest
     jest: true, // for jest
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  extends: [
-    // basic
-    'eslint:recommended',
-
-    // import
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-
-    // react
-    'plugin:react/recommended',
-
-    // prettier
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-  ],
   rules: {
-    // basic
-    'no-console': 0,
-    'func-style': [2, 'declaration', { allowArrowFunctions: true }],
-    'no-var': 2,
-    'prefer-const': 2,
-
-    // typescript
-    '@typescript-eslint/adjacent-overload-signatures': 2,
-    // opinionated
-    '@typescript-eslint/array-type': 2,
-    // require type information
-    '@typescript-eslint/await-thenable': 2,
-    // opinionated
-    '@typescript-eslint/consistent-type-assertions': 2,
-    // require type information
-    '@typescript-eslint/no-for-in-array': 2,
-    // require type information
-    '@typescript-eslint/no-implied-eval': 2,
-    // require type information
-    '@typescript-eslint/no-misused-promises': 2,
-    '@typescript-eslint/no-namespace': [2, { allowDeclarations: true }],
-    '@typescript-eslint/no-require-imports': 2,
-    // @typescript-eslint の no-unused-vars-experimental は正確ではないので, tsc の noUnused* オプションで代用する
-    // ref: https://github.com/typescript-eslint/typescript-eslint/issues/1856
-    'no-unused-vars': 0,
-    '@typescript-eslint/no-unused-vars-experimental': 0,
-    // opinionated, require type information
-    '@typescript-eslint/prefer-regexp-exec': 2,
-    // opinionated, require type information
-    '@typescript-eslint/promise-function-async': 2,
-    // require type information
-    '@typescript-eslint/require-array-sort-compare': 2,
-    // opinionated, require type information
-    '@typescript-eslint/switch-exhaustiveness-check': 2,
-    '@typescript-eslint/triple-slash-reference': 2,
-    // require type information
-    '@typescript-eslint/unbound-method': 2,
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-array-constructor.md
-    'no-array-constructor': 0,
-    '@typescript-eslint/no-array-constructor': 2,
-
-    // import
-    // eslint ではモジュールの解決に失敗することがあるので, TypeScriptに任せる
-    // ref: https://github.com/benmosher/eslint-plugin-import/issues/1341
-    'import/named': 0,
-    // default export は tsserver と相性が悪いので禁止する
-    // ref: https://typescript-jp.gitbook.io/deep-dive/main-1/defaultisbad
-    'import/no-default-export': 2,
-    // `@material-ui/{core,icons}` を直接 import するとビルドや tsserver の応答が遅くなるので,
-    // `@material-ui/{core,icons}` の import を禁止する
-    // ref: https://material-ui.com/guides/minimizing-bundle-size/#how-to-reduce-the-bundle-size
-    'no-restricted-imports': ['error', { paths: ['@material-ui/core', '@material-ui/icons'] }],
-
-    // react
-    'react/self-closing-comp': 2,
-    // propTypes は使わないので off にする
-    'react/prop-types': 0,
-    'react-hooks/rules-of-hooks': 2,
     'react-hooks/exhaustive-deps': [
       1,
       {
