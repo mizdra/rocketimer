1e30706adecb82521dca29d34f24c85d01d1fe6c
github-action-benchmark <github@users.noreply.github.com>
2020-10-11 14:47:30 +0000

    add Benchmark (benchmarkjs) benchmark result for 2984d8b8cae840d4e99e808d14a17113da42938d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602421215020,
+  "lastUpdate": 1602427650445,
   "repoUrl": "https://github.com/RNGeek/rocketimer",
   "entries": {
     "Benchmark": [
@@ -43,6 +43,51 @@ window.BENCHMARK_DATA = {
             "name": "Render Time of TimerRemainDisplay",
             "value": 1.5255041299999903,
             "range": "±14.16%",
+            "unit": "ms/render",
+            "extra": "300 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pp.mizdra@gmail.com",
+            "name": "mizdra",
+            "username": "mizdra"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2984d8b8cae840d4e99e808d14a17113da42938d",
+          "message": "Merge pull request #131 from RNGeek/improve-actions-cache\n\nactions/cache周りの整理",
+          "timestamp": "2020-10-11T23:43:43+09:00",
+          "tree_id": "2b890bb933a56be32eb7d5c9ab352a8bda466dd0",
+          "url": "https://github.com/RNGeek/rocketimer/commit/2984d8b8cae840d4e99e808d14a17113da42938d"
+        },
+        "date": 1602427649940,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "FPS during Countdowning",
+            "value": 321.71508042005905,
+            "range": "±2.99%",
+            "unit": "fps",
+            "extra": "10 samples"
+          },
+          {
+            "name": "Render Time of TimerTimeline",
+            "value": 1.1698781933334794,
+            "range": "±11.80%",
+            "unit": "ms/render",
+            "extra": "300 samples"
+          },
+          {
+            "name": "Render Time of TimerRemainDisplay",
+            "value": 1.1035796166667509,
+            "range": "±12.43%",
             "unit": "ms/render",
             "extra": "300 samples"
           }
