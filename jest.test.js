8254a38d4657009e0ca02f9f202d94a9ba480cdb
mizdra <pp.mizdra@gmail.com>
2020-10-17 16:19:58 +0900

    ディレクトリの移動に各種設定ファイルを対応させる

--- jest.test.js
+++ jest.test.js
@@ -3,7 +3,7 @@
 /** @type import('@jest/types').Config.InitialOptions */
 module.exports = {
   displayName: 'test',
-  testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
+  testMatch: ['<rootDir>/test/**/*.test.ts?(x)'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx,js,jsx}', '!**/*.d.ts'],
   moduleNameMapper: {
     '\\.mp3$': '<rootDir>/__mocks__/fileMock.js',
