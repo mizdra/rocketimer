81be91e267aae20bba28b71cc531c25848503371
mizdra <pp.mizdra@gmail.com>
2021-03-22 00:03:34 +0900

    checkSoundEvent のテストも追加

--- test/lib/timer/soudable-cascade-timer.test.ts
+++ test/lib/timer/soudable-cascade-timer.test.ts
@@ -1,4 +1,4 @@
-import { SoundableCascadeTimer } from '../../../src/lib/timer/soundable-cascade-timer';
+import { checkSoundEvent, SoundableCascadeTimer } from '../../../src/lib/timer/soundable-cascade-timer';
 import { TestableTimerController } from '../../../src/lib/timer/timer-controller';
 
 function createTimer(lapDurations: number[], offset: number, soundOffset: number) {
@@ -166,6 +166,43 @@ describe('SoundableCascadeTimer', () => {
       expect(context.ticktackEnded.mock.calls.length).toBe(0);
       context.controller.advanceBy(1); // 1ラップ目の残り時間 0 ms
       expect(context.ticktackEnded.mock.calls.length).toBe(1);
+    });
+  });
+});
+
+describe('checkSoundEvent', () => {
+  describe('`lapDurations === [11001]` の時', () => {
+    test.each([
+      [['countdowning', 11001, 0], ['countdowning', 11000, 0], undefined],
+      [['countdowning', 11001, 0], ['countdowning', 11000, 0], undefined],
+      [['countdowning', 10001, 0], ['countdowning', 10000, 0], 'ticktack'],
+      [['countdowning', 10000, 0], ['countdowning', 9999, 0], undefined],
+      [['countdowning', 1002, 0], ['countdowning', 1001, 0], undefined],
+      [['countdowning', 1001, 0], ['countdowning', 1000, 0], 'ticktack'],
+      [['countdowning', 1000, 0], ['countdowning', 999, 0], undefined],
+      [['countdowning', 2, 0], ['countdowning', 1, 0], undefined],
+      [['countdowning', 1, 0], ['ended', 0, 0], 'ticktackEnded'],
+    ] as const)('%p => %p', (prev, next, expected) => {
+      expect(
+        checkSoundEvent(
+          { status: prev[0], lapRemain: prev[1], lapIndex: prev[2], offset: 0 },
+          { status: next[0], lapRemain: next[1], lapIndex: next[2], offset: 0 },
+        ),
+      ).toBe(expected);
+    });
+  });
+  describe('`lapDurations === [1, 11000, 10000]` の時', () => {
+    test.each([
+      [['countdowning', 1, 0], ['countdowning', 11000, 1], 'ticktackEnded'],
+      [['countdowning', 1, 1], ['countdowning', 10000, 2], 'ticktackEnded'],
+      [['countdowning', 1, 2], ['ended', 0, 2], 'ticktackEnded'],
+    ] as const)('%p => %p', (prev, next, expected) => {
+      expect(
+        checkSoundEvent(
+          { status: prev[0], lapRemain: prev[1], lapIndex: prev[2], offset: 0 },
+          { status: next[0], lapRemain: next[1], lapIndex: next[2], offset: 0 },
+        ),
+      ).toBe(expected);
     });
   });
 });
