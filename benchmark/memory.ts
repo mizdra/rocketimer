e5435be8189f1b54c6f03c4c7982e4b47c998746
mizdra <pp.mizdra@gmail.com>
2021-08-14 16:23:56 +0900

    fix: fix tsc error

--- benchmark/memory.ts
+++ benchmark/memory.ts
@@ -19,7 +19,7 @@ type Measurement = {
  * */
 async function measureMemory(): Promise<Measurement[]> {
   async function wait(ms: number) {
-    return new Promise((resolve) => {
+    return new Promise<void>((resolve) => {
       setTimeout(() => resolve(), ms);
     });
   }
