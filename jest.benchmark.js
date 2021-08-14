d14fbb9b8a2caaa812bbe3abb6637971da480f34
mizdra <pp.mizdra@gmail.com>
2021-08-14 20:26:16 +0900

    chore: 明示的にトランスパイル対象を列挙しなければならなくなった

--- jest.benchmark.js
+++ jest.benchmark.js
@@ -8,4 +8,10 @@ module.exports = {
   moduleNameMapper: {
     '\\.mp3$': '<rootDir>/__mocks__/fileMock.js',
   },
+  transformIgnorePatterns: [
+    // konva はトランスパイルが必要なので明示的にトランスパイル対象に含めてやる
+    // ref: https://jestjs.io/docs/26.x/tutorial-react-native#transformignorepatterns-customization
+    '/node_modules/(?!(konva|react-performance-testing)/)',
+    '\\.pnp\\.[^\\/]+$',
+  ],
 };
