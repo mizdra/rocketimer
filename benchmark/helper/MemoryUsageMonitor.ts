49f75a8cdb5d2d290fde386c4718316f5e9434d7
mizdra <pp.mizdra@gmail.com>
2021-08-15 20:37:36 +0900

    feat: メモリ使用量を mackerel に送信する

---
+++ benchmark/helper/MemoryUsageMonitor.ts
@@ -0,0 +1,88 @@
+type MeasurementDiff = {
+  // name: BytesPerSecond
+  'memory-usage.all': number;
+  'memory-usage.breakdown.JavaScript'?: number;
+  'memory-usage.breakdown.DOM'?: number;
+  'memory-usage.breakdown.Shared'?: number;
+  'memory-usage.breakdown.Canvas'?: number;
+};
+
+export type MeasurementWithTime = {
+  time: number; // ms
+  measurement: MemoryMeasurement;
+};
+
+function generateBreakdownEntryName(entry: MemoryBreakdownEntry) {
+  return 'memory-usage.breakdown.' + entry.types.join('-');
+}
+
+function diffMeasurements(
+  { time: start, measurement: measurement1 }: MeasurementWithTime,
+  { time: end, measurement: measurement2 }: MeasurementWithTime,
+): MeasurementDiff {
+  const time = (end - start) / 1000;
+  const result: MeasurementDiff = {
+    'memory-usage.all': (measurement2.bytes - measurement1.bytes) / time,
+  };
+  measurement1.breakdown.forEach((entry1) => {
+    const name = generateBreakdownEntryName(entry1);
+    const entry2 = measurement2.breakdown.find((entry) => generateBreakdownEntryName(entry) === name);
+    if (!entry2) return;
+    if (
+      name === 'memory-usage.breakdown.JavaScript' ||
+      name === 'memory-usage.breakdown.DOM' ||
+      name === 'memory-usage.breakdown.Shared' ||
+      name === 'memory-usage.breakdown.Canvas'
+    ) {
+      result[name] = (entry2.bytes - entry1.bytes) / time;
+    }
+  });
+  return result;
+}
+
+function mean(array: number[]) {
+  return array.reduce((a, b) => a + b, 0) / array.length;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isNumber(value: any): value is number {
+  return typeof value === 'number';
+}
+
+function meanMeasurementDiffs(measurementDiffs: MeasurementDiff[]): MeasurementDiff {
+  return {
+    'memory-usage.all': mean(measurementDiffs.map((diff) => diff['memory-usage.all'])),
+    'memory-usage.breakdown.JavaScript': mean(
+      measurementDiffs.map((diff) => diff['memory-usage.breakdown.JavaScript']).filter(isNumber),
+    ),
+    'memory-usage.breakdown.DOM': mean(
+      measurementDiffs.map((diff) => diff['memory-usage.breakdown.DOM']).filter(isNumber),
+    ),
+    'memory-usage.breakdown.Shared': mean(
+      measurementDiffs.map((diff) => diff['memory-usage.breakdown.Shared']).filter(isNumber),
+    ),
+    'memory-usage.breakdown.Canvas': mean(
+      measurementDiffs.map((diff) => diff['memory-usage.breakdown.Canvas']).filter(isNumber),
+    ),
+  };
+}
+
+export function generateReport(measurementWithTimes: MeasurementWithTime[]): MeasurementDiff {
+  const measurementDiffs = [];
+  for (let i = 0; i < measurementWithTimes.length - 1; i++) {
+    const measurementWithTime1 = measurementWithTimes[i];
+    const measurementWithTime2 = measurementWithTimes[i + 1];
+    const diff = diffMeasurements(measurementWithTime1, measurementWithTime2);
+    measurementDiffs.push(diff);
+  }
+  return meanMeasurementDiffs(measurementDiffs);
+}
+
+/**
+ * @param time エポック秒
+ */
+export function printReportForMackerel(time: number, report: MeasurementDiff): void {
+  for (const [name, value] of Object.entries(report)) {
+    console.log(`${name} ${value} ${time}`);
+  }
+}
--- benchmark/helper/log.ts
+++ benchmark/helper/log.ts
@@ -4,5 +4,5 @@ import { inspect } from 'util';
 export function log(...args: any[]) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const format = (arg: any) => inspect(arg, { maxArrayLength: null, depth: null, colors: true });
-  console.log(...args.map(format));
+  console.error(...args.map(format));
 }
